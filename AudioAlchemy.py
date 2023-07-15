edf9c668e3253142d85d16c78e6221f1a76e5d1a
Paul Russell <paul.jeff.russell@gmail.com>
2023-07-15 18:53:03 -0400

    Updated the player and main app to support a card that plays all of a genre or all albums with a label

--- AudioAlchemy.py
+++ AudioAlchemy.py
@@ -10,9 +10,13 @@ import threading
 import logging
 from logging.handlers import RotatingFileHandler
 import sys
+import random
 import time
 import configfile as CONFIG
-
+import requests
+import requests
+import subprocess
+from dateutil.parser import parse
 
 
 # Create the logger
@@ -49,6 +53,11 @@ APP_RUNNING = True
 DB_CACHE_FOLDER = CONFIG.DB_CACHE_LOCATION
 
 DB_CACHE = DB_CACHE_FOLDER + "dbcache.csv"
+
+#### THESE ARE NEW
+BLANK = 'BLANK'
+
+SUPPORTED_EXTENSIONS = ['.mp3', '.MP3', '.wav', '.WAV', '.ogg', '.OGG']
 
 
 #### COMMANDS
@@ -127,50 +136,101 @@ def my_interrupt_handler(channel):
         except:
             logging.debug("DB load failed. Retrying in 3 seconds.")
             time.sleep(3)
-    """
-     
-     
+"""
 def load_database(LOAD_FROM_WEB):
-    
+    global DB
     
     DB_LOADED = False
     
     if not LOAD_FROM_WEB:
         if os.path.exists(DB_CACHE):
             logging.debug(f'Attempting to load the DB from {DB_CACHE}')
-            loaded_db = pd.read_csv(DB_CACHE)
+            DB = pd.read_csv(DB_CACHE)
             logging.debug("SUCCESS! DB loaded from cache.")
             DB_LOADED = True
-            return loaded_db
+            
+            DB['sub_genre'] = DB['sub_genre'].fillna(BLANK)
+            DB['genre'] = DB['genre'].fillna(BLANK)
+            DB['folder'] = DB['folder'].fillna(BLANK)
+            DB['labels'] = DB['labels'].fillna(BLANK)
+            
+            
+            return DB
         else:
             logging.debug("No Cached DB available. Attempting to load from web.")
             
 
     while not DB_LOADED:
         try:
+            
+            ## update the system data so that your call to google works.
+            set_ststem_date()
+            
             logging.debug("Attempting to load the DB from the internet.")
             db_url = f'https://docs.google.com/spreadsheets/d/{DB_SHEET_ID}/gviz/tq?tqx=out:csv&sheet={DB_SHEET_NAME}'
-
             logger.info(f'DB URL: {db_url}s')
             # Read the spreadsheet
         
-            loaded_db = pd.read_csv(db_url)
+            #logger.debug('This is the response for the URL.')
+            #response = requests.get(db_url)
+
+            
+            #logger.debug(response)
+            #logger.debug("\n\n")
+        
+            logger.debug('ready to read the csv from url.')
+            DB = pd.read_csv(db_url)
             logging.debug("SUCCESS! The DB loaded from the web.")
             
+
+            
             backup_cache()
-            loaded_db.to_csv(DB_CACHE, index=False)
-                
+            DB.to_csv(DB_CACHE, index=False)
+            DB['sub_genre'] = DB['sub_genre'].fillna(BLANK)
+            DB['genre'] = DB['genre'].fillna(BLANK)
+            DB['folder'] = DB['folder'].fillna(BLANK)
+            DB['labels'] = DB['labels'].fillna(BLANK)
+            
+            
             DB_LOADED = True
-            return loaded_db
-        except:
-            #logging.debug(f'An exception occurred: {str(e)}')
+            return DB
+        except Exception as e:
+            logging.debug(f'An exception occurred:')
+            logging.debug(str(e))
             logging.debug("DB load failed. Retrying in 3 seconds.")
             time.sleep(3)
 
+
+
+"""
+    def set_ststem_date():
+  
+    because the system is read only, every time you reboot the system thinks the date is 
+            June 17 2023 which is when OS was set to read only.
             
-        
+            intenet requests to google fail if the system date is off too much because the system
+"""      
+def set_ststem_date():
+
+    logging.debug('Starting set_ststem_date()')
+    response = requests.get('http://worldtimeapi.org/api/timezone/America/New_York')
+    data = response.json()
+
+    # Parsing the datetime string
+    dt = parse(data['datetime'])
+
+    # Formatting date time as required by the 'date' command
+    formatted_dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Form the command
+    cmd = f"date -s '{formatted_dt}'"
+
+    logging.debug(f'Setting system date {cmd}')
+    # Execute the command
+    subprocess.run(['sudo', 'bash', '-c', cmd])    
     
 def backup_cache():
+    logging.debug('Starting backup_cache().')
     logging.debug(f'Attempting to backup existing cache {DB_CACHE}')
     if os.path.exists(DB_CACHE):
         # Create a timestamp for the backup file name
@@ -244,6 +304,7 @@ def lookup_field_by_field(df, search_column, search_term, result_column):
         logger.warning(f'The value {search_term} was not found in the column {search_column}.')
         return 0
 
+
           
 """
 Return true if the album is set to shuffle
@@ -252,7 +313,7 @@ Return true if the album is set to shuffle
 def is_album_shuffle(rfid_code):
     global DB
     
-    suffle_album = lookup_field_by_field(DB, 'rfid', rfid_code, 'shuffle')  
+    suffle_album = lookup_field_by_field(DB, 'rfid', rfid_code, 'shuffle_songs')  
     if suffle_album == 1:
         return True
     else:
@@ -270,10 +331,187 @@ def is_album_repeat(rfid_code):
         return True
     else:
         return False
-                 
-                 
+
+
+
+"""
+Returns a list of the paths to the library folders that match the provided genre and sub_genre
+"""
+def get_albums_for_genre(genre, sub_genre):
+          
+    if (len(genre) != 0 and len(sub_genre) != 0):      
+        condition = "genre == '" + genre + "' and sub_genre == '" + sub_genre + "'"   
+        logger.debug(f'Looking up tracks for genre: {genre}:{sub_genre}...')   
+    elif (len(genre) == 0 and len(sub_genre) != 0):     
+        condition = "sub_genre == '" + sub_genre + "'"
+        logger.debug(f'Looking up tracks for sub genre: {sub_genre}...') 
+    elif (len(genre) != 0 and len(sub_genre) == 0): 
+        condition = "genre == '" + genre + "'"
+        logger.debug(f'Looking up tracks for genre: {genre}...') 
+    else:
+        condition = "genre == '" + BLANK + "' and sub_genre == '" + BLANK + "'"
+        logger.debug('Looking up tracks for genre: '+BLANK+':'+BLANK )  
+        
+        ## add this so the app doesn't choke if the folder for the genre tag is empty
+        condition = condition + " and folder != '" +  BLANK + "'"
+        
+    logger.debug(f'Attemptting to play: {condition}...')
+
+    # Use the query method to filter the DataFrame and extract the 'folder' column
+    matching_folders = DB.query(condition)['folder']
+
+    # Convert the matching folders to a list
+    matching_folders_list = matching_folders.tolist()
+    matching_folders_list = [LIBRARY_CACHE_FOLDER + folder for folder in matching_folders_list]
+
+    return matching_folders_list
+
+
+def get_albums_for_label(label):
+
+    logger.debug(f'Looking up albums with label: {label}...')
+
+    #matching_rows = DB[DB['labels'].apply(lambda x: label in x)]
+    matching_rows = DB[DB['labels'].astype(str).str.contains(label)]
+    #DB.query(condition)['folder']
+    matching_folders = matching_rows.query("folder != '"+BLANK+"'")['folder']
+    
+    # Convert the matching folders to a list
+    matching_folders_list = matching_folders.tolist()
+        
+        
+    matching_folders_list = [LIBRARY_CACHE_FOLDER + folder for folder in matching_folders_list]
+
+    return matching_folders_list
+      
+    
+    
+
+
+"""
+Returns a list of the full path to tracks for a list of folders. If  shuffle_tracks == True, the resulting
+    list of tracks will be randomly shuffled.
+"""
+def get_tracks(folders, shuffle_tracks):
+    global SUPPORTED_EXTENSIONS
+    
+    all_tracks = []
+    
+    #shuffle the albums so we don't always start with the same album
+    random.shuffle(folders)
+    
+    for folder in folders:
+        #folder_path = os.path.join(base_folder_path, folder)  # Assuming a base folder path
+    
+        if os.path.exists(folder):
+            logger.debug(f'Getting files for {folder}...')
+            album_songs = [os.path.join(folder, f) for f in os.listdir(folder) if os.path.splitext(f)[1] in SUPPORTED_EXTENSIONS]
+            album_songs.sort()
+            all_tracks.extend(album_songs)
+        
+    if(shuffle_tracks == True):
+        random.shuffle(all_tracks)  
+    
+    
+    #print (all_tracks)
+    for mp3_file in all_tracks:
+        logger.debug(f'Found Track: {mp3_file}...') 
+
+    return all_tracks   
+
+
+
 def handle_rfid_read(rfid_code):
-        global DB, THREAD_LOCK
+    global DB
+    
+    
+    genre_card = lookup_field_by_field(DB, 'rfid', rfid_code, 'genre_card')
+
+    label_card = lookup_field_by_field(DB, 'rfid', rfid_code, 'label_card')
+    #except:
+    #    logger.warning("Could not load genre or label card on RFID read.")
+    
+    if (command_card_handler(rfid_code) == True):
+        ## You found a command card. It's been executed. Don't do anything else.
+        logger.debug(f'Completed command card: {rfid_code}...')
+        
+    elif (genre_card == 1):
+        ## this card is meant to play a genre instead of a specific album
+        genre = lookup_field_by_field(DB, 'rfid', rfid_code, 'genre')
+        sub_genre = lookup_field_by_field(DB, 'rfid', rfid_code, 'sub_genre')
+        
+        #get the list of all the folders from this genre
+        genre_album_folder_list = get_albums_for_genre(genre, sub_genre)
+        
+        tracks = get_tracks(genre_album_folder_list, is_album_shuffle(rfid_code))
+        
+        if (len(tracks) > 0):
+            logger.debug (f'Album folder exists. Playing Genre')
+            aaplayer.play_tracks(tracks, is_album_repeat(rfid_code) )   
+    
+    elif (label_card == 1):
+        #this card is a label card. It is intended to play all of the albums with a matching lable.
+        label = lookup_field_by_field(DB, 'rfid', rfid_code, 'labels')
+        label_album_folder_list = get_albums_for_label(label)
+        tracks = get_tracks(label_album_folder_list, is_album_shuffle(rfid_code))
+        
+        if (len(tracks) > 0):
+            logger.debug (f'Album folder exists. Playing Genre')
+            aaplayer.play_tracks(tracks, is_album_repeat(rfid_code) )
+    
+    else:
+        album_folder_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'folder')
+        logger.debug(f'Attemptting to play album: {album_folder_name}...')
+
+        album_folder = LIBRARY_CACHE_FOLDER + album_folder_name
+            
+        tracks = get_tracks([album_folder], is_album_shuffle(rfid_code))
+
+        if (len(tracks) > 0):
+            logger.debug (f'Album has tracks. Playing...')
+            aaplayer.play_tracks(tracks, is_album_repeat(rfid_code) )
+        else:
+            logger.warning (f'No tracks found for folder {album_folder}.')    
+
+      
+        
+        
+        
+        
+
+          
+def handle_rfid_read_old(rfid_code):
+    global DB, THREAD_LOCK
+    
+    #THREAD_LOCK.acquire()
+    
+    logger.debug(f'Looking up album folder for RFID: {rfid_code}...')
+
+    ## check if you recieved a command card
+    ## Command cards handle system wide settings 
+    if (command_card_handler(rfid_code) == True):
+        logger.debug(f'Completed command card: {rfid_code}...')      
+    else:
+        ##album_folder_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'folder')
+        
+        album_folder_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'folder')
+        logger.debug(f'Attemptting to play {album_folder_name}...')
+
+        if (album_folder_name != 0):
+            album_folder = LIBRARY_CACHE_FOLDER + album_folder_name
+
+            logger.debug(f'Looking for {album_folder}...')
+            
+            if os.path.exists(album_folder):
+                #if os.path.isdir(folder_name): 
+                logger.debug (f'Album folder exists. Playing {album_folder_name}')
+                aaplayer.play_folder( album_folder, is_album_shuffle(rfid_code), is_album_repeat(rfid_code) )
+            else:
+                logger.warning (f'Folder {album_folder} does not exist.')
+                
+             
+"""def handle_rfid_read(rfid_code):
+        global DB
         
         #THREAD_LOCK.acquire()
         
@@ -286,22 +524,50 @@ def handle_rfid_read(rfid_code):
         else:
             ##album_folder_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'folder')
             
-            album_folder_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'folder')
-            logger.debug(f'Attemptting to play {album_folder_name}...')
-    
-            if (album_folder_name != 0):
-                album_folder = LIBRARY_CACHE_FOLDER + album_folder_name
-
-                logger.debug(f'Looking for {album_folder}...')
-                
-                if os.path.exists(album_folder):
-                    #if os.path.isdir(folder_name): 
-                    logger.debug (f'Album folder exists. Playing {album_folder_name}')
-                    aaplayer.play_folder( album_folder, is_album_shuffle(rfid_code), is_album_repeat(rfid_code) )
+          
+            
+            genere_card = lookup_field_by_field(DB, 'rfid', rfid_code, 'genere_card')
+            
+            if (genere_card = 1):
+                ## this card is meant to play a genre instead of a specific album
+                genre_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'genre')
+                sub_genre_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'sub_genre')
+                if (len(sub_genre_name) == 0):
+                    logger.debug(f'Attemptting to play genre: {genre_name}...')
                 else:
-                    logger.warning (f'Folder {album_folder} does not exist.')
+                    logger.debug(f'Attemptting to play genre: {genre_name}:{sub_genre_name}...')
+                    
+                
+                
+                #get the list of all the folders from this genre
+                genre_album_folder_list = get_albums_for_genre(genre_full_name)
+                
+                
+                ## play all 
+                aaplayer.play_genre(genre_album_list, is_album_shuffle(rfid_code), is_album_repeat(rfid_code))
+                
+                
+            else:
+                ## This card is meant to play a single album
+                
+                album_folder_name = lookup_field_by_field(DB, 'rfid', rfid_code, 'folder')
+                logger.debug(f'Attemptting to play album: {album_folder_name}...')
+    
+                if (album_folder_name != 0):
+                    album_folder = LIBRARY_CACHE_FOLDER + album_folder_name
+
+                    logger.debug(f'Looking for {album_folder}...')
+                
+                    if os.path.exists(album_folder):
+                        #if os.path.isdir(folder_name): 
+                        logger.debug (f'Album folder exists. Playing {album_folder_name}')
+                        aaplayer.play_folder( album_folder, is_album_shuffle(rfid_code), is_album_repeat(rfid_code) )
+                    else:
+                        logger.warning (f'Folder {album_folder} does not exist.')
                     
         
+"""
+
 
           
          
@@ -340,28 +606,7 @@ def start_rfid_reader():
 
 
 def start_button_controls():
-    """GPIO.setwarnings(False) # Ignore warning for now
-    GPIO.setmode(GPIO.BOARD ) # Use physical pin numbering
-    
-    if (CONFIG.BUTTON_HIGH):
-    
-        GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-        GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-        GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-
-        GPIO.add_event_detect(16,GPIO.RISING,callback=button_callback_16, bouncetime=200) # Setup event on pin 10 rising edge
-        GPIO.add_event_detect(15,GPIO.RISING,callback=button_callback_15, bouncetime=200) # Setup event on pin 10 rising edge
-        GPIO.add_event_detect(13,GPIO.RISING,callback=button_callback_13, bouncetime=200) # Setup event on pin 10 rising edge
-    else:
-        GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-        GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-        GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-
-        GPIO.add_event_detect(16,GPIO.FALLING,callback=button_callback_16, bouncetime=200) # Setup event on pin 10 rising edge
-        GPIO.add_event_detect(15,GPIO.FALLING,callback=button_callback_15, bouncetime=200) # Setup event on pin 10 rising edge
-        GPIO.add_event_detect(13,GPIO.FALLING,callback=button_callback_13, bouncetime=200) # Setup event on pin 10 rising edge
-        
-    """   
+ 
     button16 = Button("BOARD16")
     button15 = Button("BOARD15")
     button13 = Button("BOARD13")
@@ -379,9 +624,21 @@ def main():
     #try:
     # Load the database of RFID tags and their matching albums
     DB = load_database(False)
-
+    
+    #DB['sub_genre'] = DB['sub_genre'].fillna(BLANK)
+    #DB['genre'] = DB['genre'].fillna(BLANK)
+    #DB['folder'] = DB['folder'].fillna(BLANK)
+    #DB['labels'] = DB['labels'].fillna(BLANK)
+    
+    columns_to_print = ['folder', 'genre', 'sub_genre']
+    print(DB[columns_to_print])
+    #except:
+    #    logger.warning('Failed to update blank columns.')
+    
+   
+    
     #with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-    print(DB)
+    #print(DB)
 
     
 
